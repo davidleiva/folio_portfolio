daffe54b182c5b65f5cf92c7ed3b8c2d3c3bc5f6
David Leiva <david.leiva@getlife.es>
2021-12-22 13:28:36 +0100

    remove text decoration logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import { Title } from '../Core'
 
 const Logo = ({ color = 'front', height, className = '', ...rest }) => {
     return (
-        <Link to="/" className={`${className}`} {...rest}>
+        <Link to="/" className={`text-decoration-none ${className}`} {...rest}>
             <Title color={color} variant="cardLg" className="mb-0">
                 Hello :) I am David Leiva.
             </Title>
